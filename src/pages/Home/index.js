26c89256fdd1236c7adf3aaf804573b3140e9788
renatopcinfo <renatopcinfo@gmail.com>
2020-08-02 18:17:37 -0300

    readme

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import logo from '../../assets/logo-nav.png'
 import lupa from '../../assets/ic-search-copy.png'
 import close from '../../assets/ic-close.png'
